349fc7efe790df82454b8bfd3440745392941fd0
warrapaul <warapaul@gmail.com>
2024-03-28 21:38:07 +0300

    current vs

--- src/common/guards/permissions.guard.ts
+++ src/common/guards/permissions.guard.ts
@@ -6,8 +6,14 @@ import { PERMISSIONS_KEY } from '../../common/decorators/permissions.decorator';
 @Injectable()
 export class PermissionsGuard implements CanActivate {
   constructor(private reflector:Reflector){}
+  
   canActivate(context: ExecutionContext): boolean{
-    const requiredPermissions = this.reflector.getAllAndOverride<string[]>(PERMISSIONS_KEY, [
+    // const requiredPermissions = this.reflector.getAllAndOverride<string[]>(PERMISSIONS_KEY, [
+    //   context.getHandler(),
+    //   context.getClass(),
+    // ]);
+
+    const requiredPermissions = this.reflector.getAllAndMerge<string[]>(PERMISSIONS_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
